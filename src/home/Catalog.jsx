024bf3bf97815dea5f823abc098069036e466b10
Morgan Hondros <morganhondros@gmail.com>
2024-01-27 19:27:57 -0500

    fixed a whole bunch of stuff I broke.

--- src/home/Catalog.jsx
+++ src/home/Catalog.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Catalog.css"; // Import the CSS file
+import "./Catalog3.css"; // Import the CSS file
 
 
 
-const Catalog2 = ({ list, ChangeFromParent }) => {
+const Catalog3 = ({ list, ChangeFromParent }) => {
   return (
-    <div className="portfolio-area pt--90 pb--140 bg_color--1" style={{ backgroundColor: '#f3edd1', paddingBottom: '0px', paddingTop: '0' }}>
+    <div className="portfolio-area pt--90 pb--140 bg_color--1" style={{ backgroundColor: '#f3edd1', paddingBottom: '20px', paddingTop: '0' }}>
       {/* ... other code ... */}
       <div className="row" style={{ backgroundColor: '#f3edd1', paddingTop: '0px' }}>
         {list.map((value, i) => (
-          // <div key={i} className='col-lg-6 col-md-6 col-12' style={{ cursor: 'pointer', marginTop:'20px' }} onClick={() => window.location.href = value.link}>
-           <div key={i} className='col-lg-6 col-md-6 col-12' style={{ cursor: 'pointer', marginTop:'20px' }}>
-            {/* <div className="blog blog-style--1" style={{ backgroundColor: '#f3edd1', borderRadius: '20px', height: '100%' }}> */}
-            <div className="blog" style={{ backgroundColor: '#f3edd1', borderRadius: '20px', height: '100%' }}>
-
-              {/* <a className="blogtype" href="/parksingle2"> */}
-              <a className="blogtype" href="/drycamo2">
-              {/* <div className="thumbnail" style={{ backgroundColor: '#f3edd1', height: '100%' }}> */}
-              <div className="" style={{ backgroundColor: '#f3edd1', height: '100%' }}>
-                <img className="w-100" src={value.url} alt="Blog Images" />
-                {/* <img className="w-100" src="uscolorway2" alt="Blog Images" /> */}
+          // <div key={i} className='col-lg-4 col-md-6 col-12' style={{ cursor: 'pointer', marginTop:'20px' }} onClick={() => window.location.href = value.link}>
+          <div
+  key={i}
+  className='col-lg-6 col-md-6 col-12'
+  style={{ cursor: 'pointer', marginTop:'20px' }}
+  onClick={() => window.location.href = '/drycamo2'}
+>
+            <div className="blog blog-style--1" style={{ backgroundColor: '#f3edd1', borderRadius: '20px', height: '100%' }}>
+              <div className="thumbnail" style={{ backgroundColor: '#f3edd1', height: '100%' }}>
+                <img className="w-100 merchImage" src={value.url} alt="Blog Images" />
+                {/* <img className="merchImage" src={value.url} alt="Blog Images" /> */}
+                {/* w-100 */}
               </div>
-              </a>
               <div className="content">
-                <p className="blogtype" style={{ color: '#EF5474', fontSize: '2.0rem', paddingBottom: '0px', fontFamily: 'Brice-Black', textAlign: 'center', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)' }}><a href="/drycamo2">{value.category}</a></p>
+                <p className="blogtype" style={{ color: '#EF5474', fontSize: '2.0rem', paddingBottom: '0px', textAlign: 'center',  }}>{value.category}</p>
+                {/* textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)' */}
                 <div className="blog-btn">
                   {/* <span className="rn-btn text-white">Read More</span> */}
                 </div>
@@ -38,4 +39,4 @@ const Catalog2 = ({ list, ChangeFromParent }) => {
   );
 };
 
-export default Catalog2;
+export default Catalog3;
